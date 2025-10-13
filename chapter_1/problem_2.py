d1f3938c33733aefa1428384d10ee5a0b2b72825
mobashshirhasan <mobashshir12@gmail.com>
2025-10-13 23:27:26 +0530

    first commit

---
+++ chapter_1/problem_2.py
@@ -0,0 +1,13 @@
+""" Print table of 5"""
+
+print("Tbale of 5: ")
+print(f"5 X 1 = ", 5 * 1)
+print(f"5 X 2 = ", 5 * 2)
+print(f"5 X 3 = ", 5 * 3)
+print(f"5 X 4 = ", 5 * 4)
+print(f"5 X 5 = ", 5 * 5)
+print(f"5 X 6 = ", 5 * 6)
+print(f"5 X 7 = ", 5 * 7)
+print(f"5 X 8 = ", 5 * 8)
+print(f"5 X 9 = ", 5 * 9)
+print(f"5 X 10 = ", 5 * 10)
---
+++ chapter_1/problem_4.py
@@ -0,0 +1,14 @@
+# """ write a program to print content of the directory.  """
+
+# import os
+
+# directory_path = '/'
+
+# contents  = os.listdir(directory_path)
+
+# for item in contents:
+
+#     print(contents)
+
+#print("my name is Tarique ,i am worst nightmare for nonsense person")
+
