9ebea537c2c0aab75f637d24dab9a02b5d43c0b9
mobashshirhasan <mobashshir12@gmail.com>
2025-10-13 23:52:11 +0530

    leap year

---
+++ conditional_statement/leap_year.py
@@ -0,0 +1,14 @@
+""" leap Year """
+
+year = int(input("Enter a year :"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"Your {year} is leap year") 
+        else:
+            print(f"{year} is not a leap year. ")
+    else:
+        print(f"{year} is leap year.")
+else:
+    print(f"{year} is not a leap year. ")
